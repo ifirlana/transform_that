46f9ebeac2d1565f8692cf961a3d0f5bf03692f9
ikhlas firlana <ifirlana@gmail.com>
2017-09-07 21:35:09 +0700

    no message

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by ikhlasfirlana on 9/7/17.
+ */
+const Promise = require("promise");
+
+
+// object json { ... }
+// exist string "hello.world.test"
+exports.look_string_in_object = function (object, that_exist) {
+    "use strict";
+
+    return new Promise(function (resolve, reject) {
+
+        try {
+
+            var this_is_exist = object;
+
+            if (typeof that_exist === "string") {
+
+                var tmp = that_exist.split(".");
+
+                for (var i in tmp) {
+
+                    if (typeof this_is_exist !== "undefined") {
+
+                        this_is_exist = this_is_exist[tmp[i]];
+
+                    }
+
+                }
+            }
+
+            if (typeof this_is_exist !== "undefined") {
+
+                resolve(this_is_exist);
+
+            } else {
+
+                resolve();
+
+            }
+
+        } catch (e) {
+
+            // console.log(":look_string_in_object:run_exist e", e);
+            reject(e);
+
+        }
+    });
+
+};
